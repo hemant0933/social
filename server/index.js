81ed7b47635063de88d6eaa4ac82c5daba4f183d
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-09-13 22:45:11 +0530

    origin

--- server/index.js
+++ server/index.js
@@ -41,10 +41,10 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 //enabling CORS for all requests
 
 app.use(cors(
-    // {
-    //     origin:[`http//localhost:${process.env.PORT}`],
-    //     methods:["GET", "POST", "PATCH"],
-    // }
+    {
+        origin:[`https://social-pvx3.onrender.com`],
+        methods:["GET", "POST", "PATCH"],
+    }
 ));
 
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
